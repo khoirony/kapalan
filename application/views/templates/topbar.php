b70a6d61b95a5f4a6fa5859b0f1a703bf734cdda
Khoirony Arief <khoironyarief08@gmail.com>
2022-01-08 21:15:41 +0700

    update

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -13,6 +13,13 @@
             </button>
 
             <!-- Topbar Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="javascript:history.back()">Back</a>
+                </li>
+
+            </ul>
             <ul class="navbar-nav ml-auto">
                 <div class="topbar-divider d-none d-sm-block"></div>
                 <?php
@@ -23,13 +30,14 @@
                 }
                 ?>
 
-
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
+
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['nama']; ?></span>
                         <img class="img-profile rounded-circle" src="<?= base_url('assets/img/profile/') . $profile; ?>">
                     </a>
+
                     <!-- Dropdown - User Information -->
                     <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                         <a class="dropdown-item" href="#">
